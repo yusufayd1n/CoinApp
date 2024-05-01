e3f1b9d039b7cd4cb020edf23642343c28f85439
yusufayd1n <yusufaydn24@gmail.com>
2024-05-01 18:36:30 +0300

    Coin model changed

--- app/src/main/java/com/example/coinapp/model/Coin.kt
+++ app/src/main/java/com/example/coinapp/model/Coin.kt
@@ -1,8 +1,40 @@
 package com.example.coinapp.model
 
-import android.os.Parcelable
-
-@kotlinx.parcelize.Parcelize
 data class Coin(
-    val id : String = "123"
-) : Parcelable
+    val `data`: Data,
+    val status: String
+) {
+    data class Data(
+        val coins: List<Coin>,
+        val stats: Stats
+    ) {
+        data class Coin(
+            val `24hVolume`: String,
+            val btcPrice: String,
+            val change: String,
+            val coinrankingUrl: String,
+            val color: String,
+            val contractAddresses: List<String>,
+            val iconUrl: String,
+            val listedAt: Int,
+            val lowVolume: Boolean,
+            val marketCap: String,
+            val name: String,
+            val price: String,
+            val rank: Int,
+            val sparkline: List<String>,
+            val symbol: String,
+            val tier: Int,
+            val uuid: String
+        )
+
+        data class Stats(
+            val total: Int,
+            val total24hVolume: String,
+            val totalCoins: Int,
+            val totalExchanges: Int,
+            val totalMarketCap: String,
+            val totalMarkets: Int
+        )
+    }
+}
