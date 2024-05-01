e95a7e96b636e6f27f6747f9a3857b9bf99ce6c9
yusufayd1n <yusufaydn24@gmail.com>
2024-05-01 15:40:33 +0300

    CoinsFragment and recyclerview item ui done with dummy datas

--- app/src/main/java/com/example/coinapp/view/CoinsFragment.kt
+++ app/src/main/java/com/example/coinapp/view/CoinsFragment.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.coinapp.CoinsFragmentDirections
 import com.example.coinapp.databinding.FragmentCoinsBinding
-import com.example.coinapp.model.Coin
 
 class CoinsFragment : Fragment() {
 
@@ -22,17 +19,11 @@ class CoinsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.setOnClickListener {
-            val action =
-                CoinsFragmentDirections.actionCoinsFragmentToCoinDetailFragment(Coin("6161"))
-            findNavController().navigate(action)
-        }
     }
 
 }
